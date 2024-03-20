5f1fab2b99cff6d1a2095c0134f8c0087c98a77f
Ridoy Sharif <wdridoy11@gmail.com>
2024-03-20 10:50:14 +0600

    live chat install successful

--- src/LiveChat.jsx
+++ src/LiveChat.jsx
@@ -1,16 +1,3 @@
-# How to add live chat feature in your website using React
-
-### Step 1:
-Open your React project or create
-
-### Step 2: 
-Open your terminal and install Enter this command
-command: npm i react-messenger-customer-chat
-
-### Step 3: 
-Create one page like: LiveChat.jsx
-Past this code in your "LiveChat" page
-
 import React from 'react'
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
