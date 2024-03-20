f8b2e09b08167f3fa2a1d3e2594c0351b6230300
Ridoy Sharif <wdridoy11@gmail.com>
2024-03-20 10:40:17 +0600

    react install successful

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="#">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="#">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Welcome to my website</h1>
+    </>
+  )
+}
+
+export default App
